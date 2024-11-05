4496e022d1110b0bf3e83e0aa629e0d290dc4930
RuddyUNI <rc21-1826@unphu.edu.do>
2024-11-05 17:00:27 -0400

    Final

--- TorneoVideoJuegos-main/torneo/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Evento
-
-class EventoForm(forms.ModelForm):
-    class Meta:
-        model = Evento
-        fields = ['titulo', 'fecha', 'juego', 'descripcion']
